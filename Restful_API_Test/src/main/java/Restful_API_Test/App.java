d664a893603023abecfb35f815ae6a3675b24e35
Raeed Salah <Raeed Salah@192.168.1.8>
2021-06-28 13:32:26 +0200

    Commit

---
+++ Restful_API_Test/src/main/java/Restful_API_Test/App.java
@@ -0,0 +1,13 @@
+package Restful_API_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
